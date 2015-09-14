14c983b61af99e0679b0e9e26fb915209cd611fe
Shadi_Haque <Shadid-Haque@users.noreply.github.com>
2015-09-14 12:07:31 -0400

    Update Lab1-Step-36.php

--- Lab1-Step-36.php
+++ Lab1-Step-36.php
@@ -5,9 +5,17 @@ $c1 = $_POST ["c"];
 $d1 = $_POST ["d"];
 $e1 = $_POST ["e"];
 
+
 echo "<p>Greetings $a1 $b1</p>";
 echo "<p>Your User name is: $c1 and your password is: $d1</p>";
-echo "<p>your cellphone number is: $e1 </P>";
+// echo "<p>your cellphone number is: $e1 </P>";
+
+// printing the cellphone number 
+
+echo "<p>your cellphone number is: "
+echo substr($e1,0,3)."-" ;
+echo substr($e1,3,3)."-"; 
+echo substr($e1,6);
 
 echo "<p><hr color='darkblue' size='4'>End of Part3</p>";
 ?>
