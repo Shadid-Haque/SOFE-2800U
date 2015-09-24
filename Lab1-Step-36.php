b014a69086829c896a55a76b1c81d2b1a04348bf
Shadi_Haque <Shadid-Haque@users.noreply.github.com>
2015-09-24 18:51:22 -0400

    Update Lab1-Step-36.php

--- Lab1-Step-36.php
+++ Lab1-Step-36.php
@@ -10,10 +10,10 @@ echo "<p>Greetings $a1 $b1</p>";
 echo "<p>Your User name is: $c1 and your password is: $d1</p>";
 // echo "<p>your cellphone number is: $e1 </P>";
 
-// printing the cellphone number 
+// printing the cellphone number in a apropiate format
 
-echo "<p>your cellphone number is: "
-echo substr($e1,0,3)."-" ;
+echo "<p>your cellphone number is: ";
+echo substr($e1,0,3)."-";
 echo substr($e1,3,3)."-"; 
 echo substr($e1,6);
 
